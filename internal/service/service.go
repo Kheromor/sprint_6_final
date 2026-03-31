f9e0e5bb3d21ad781f4d35f24401f3a9a6918455
Danila Kaloshin <kaloshinds@gmail.com>
2026-03-31 17:29:24 +0300

    sixth commit

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"regexp"
 	"strings"
 
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
@@ -12,12 +13,28 @@ func DetectAndConvert(data string) (string, error) {
 		return "", nil
 	}
 
+	// Проверяем наличие русских букв
 	for _, char := range data {
 		if (char >= 'а' && char <= 'я') || (char >= 'А' && char <= 'Я') {
 			return morse.ToMorse(data), nil
 		}
 	}
 
-	normalized := strings.Join(strings.Fields(data), " ")
-	return morse.ToText(normalized), nil
+	wordRegex := regexp.MustCompile(`\s{2,}`)
+	words := wordRegex.Split(data, -1)
+
+	var normalizedWords []string
+	for _, word := range words {
+		// Убираем лишние пробелы внутри слова
+		symbols := strings.Fields(word)
+		if len(symbols) > 0 {
+			normalizedWords = append(normalizedWords, strings.Join(symbols, " "))
+		}
+	}
+
+	// Собираем с тремя пробелами между словами (требование пакета morse)
+	normalizedMorse := strings.Join(normalizedWords, "   ")
+
+	// Конвертируем в текст
+	return morse.ToText(normalizedMorse), nil
 }
